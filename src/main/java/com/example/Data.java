8619f55b4e1d504e75d5de975d663166b9ec277e
Ella Yurochko <bcerga.ycnex@yandex.com>
2023-07-11 23:38:36 +0300

    added files Data, User, SimpleExample, CheckUserDataTest

---
+++ src/main/java/com/example/Data.java
@@ -0,0 +1,53 @@
+package com.example;
+
+public class Data {
+
+        // опиши все поля
+        private String name;
+        private String about;
+        private String avatar;
+        private String _id;
+        private String email;
+
+        // добавь геттеры и сеттеры
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getAbout() {
+            return about;
+        }
+
+        public void setAbout(String about) {
+            this.about = about;
+        }
+
+        public String getAvatar() {
+            return avatar;
+        }
+
+        public void setAvatar(String avatar) {
+            this.avatar = avatar;
+        }
+
+        public String get_id() {
+            return _id;
+        }
+
+        public void set_id(String _id) {
+            this._id = _id;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+}

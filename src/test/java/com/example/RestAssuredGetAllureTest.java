d95eb8eb333ef34713bf6c96b6abe85953443f3f
Ella Yurochko <bcerga.ycnex@yandex.com>
2023-07-13 19:03:56 +0300

    fixed files POM

--- src/test/java/com/example/RestAssuredGetAllureTest.java
+++ src/test/java/com/example/RestAssuredGetAllureTest.java
@@ -1,4 +1,7 @@
 package com.example;
+import io.qameta.allure.Description;
+import io.qameta.allure.Step;
+import io.qameta.allure.junit4.DisplayName;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.junit.Before;
@@ -17,6 +20,8 @@ public class RestAssuredGetAllureTest {
     }
 
     @Test
+    @DisplayName("Check status code of /users/me") // имя теста
+    @Description("Basic test for /users/me endpoint") // описание теста
     public void getMyInfoStatusCode() {
         given()
                 .auth().oauth2(bearerToken)
@@ -25,6 +30,8 @@ public class RestAssuredGetAllureTest {
     }
 
     @Test
+    @DisplayName("Check user name")
+    @Description("Checking user name is very important") // описание теста
     public void checkUserName() {
         given()
                 .auth().oauth2(bearerToken)
@@ -33,16 +40,38 @@ public class RestAssuredGetAllureTest {
     }
 
     @Test
+    @DisplayName("Check user name and print response body") // имя теста
+    @Description("This is a more complicated test with console output") // описание теста
     public void checkUserNameAndPrintResponseBody() {
 
-        Response response =given().auth().oauth2(bearerToken).get("/api/users/me");
+        Response response = sendGetRequestUsersMe();
         // отправили запрос и сохранили ответ в переменную response - экземпляр класса Response
 
-        response.then().assertThat().body("data.name",equalTo("Василий Васильев"));
+        compareUserNameToText(response, "Василий Васильев");
         // проверили, что в теле ответа ключу name соответствует нужное имя пользователя
 
-        System.out.println(response.body().asString()); // вывели тело ответа на экран
+        printResponseBodyToConsole(response); // вывели тело ответа на экран
 
     }
+
+    // метод для шага "Отправить запрос":
+    @Step("Send GET request to /api/users/me")
+    public Response sendGetRequestUsersMe(){
+        Response response =given().auth().oauth2(bearerToken).get("/api/users/me");
+        return response;
+    }
+
+    // метод для шага "Сравнить имя пользователя с заданным":
+    @Step("Compare user name to something")
+    public void compareUserNameToText(Response response, String username){
+        response.then().assertThat().body("data.name",equalTo(username));
+    }
+
+    // метод для шага "Вывести тело ответа в консоль":
+    @Step("Print response body to console")
+    public void printResponseBodyToConsole(Response response){
+        System.out.println(response.body().asString());
+    }
+
 
 }
